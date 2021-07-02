fbd3d7e12b060c45a279f574be8e27e35818ed50
florian schweizer <florian.schweizer@tum.de>
2021-07-02 21:24:11 +0200

    Initial Commit

---
+++ Bundesliga Tracker/Bundesliga_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bundesliga_TrackerApp.swift
+//  Bundesliga Tracker
+//
+//  Created by florian schweizer on 02.07.21.
+//
+
+import SwiftUI
+
+@main
+struct Bundesliga_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bundesliga Tracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bundesliga Tracker
+//
+//  Created by florian schweizer on 02.07.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
